00a58538daca23a70cbb5d07b47134c96cc8af93
Afroza Nowshin Senjuti <afroza.nowshin@adndiginet.com>
2023-10-23 17:01:43 +0600

    added view for 10 coolests districts api with url

--- apilist/urls.py
+++ apilist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.UserRegistration.as_view(), name='register'),
     path('login/', views.UserLogin.as_view(), name='login'),
-    path('user-list/', views.UserListAPIView.as_view(), name='user-list')
+    path('user-list/', views.UserListAPIView.as_view(), name='user-list'),
+    path('coolest-districts/', views.CoolestDistrictsAPIView.as_view(), name='coolest-districts')
 ]
--- apilist/views.py
+++ apilist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, status
+import requests
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -54,3 +55,61 @@ class UserListAPIView(APIView):
         users = CustomUser.objects.all()
         serializer = UserSerializer(users, many=True)  # UserSerializer for displaying user details
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class CoolestDistrictsAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        try:
+            
+            response = requests.get('https://raw.githubusercontent.com/strativ-dev/technical-screening-test/main/bd-districts.json')
+            response.raise_for_status()  
+
+            districts_data = response.json().get('districts', [])  
+
+            coolest_districts = []
+
+            
+            for district in districts_data:
+                latitude = district.get('lat')  
+                longitude = district.get('long')  
+
+                if latitude is not None and longitude is not None:
+                    api_url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&timezone=GMT'
+
+                    # print(f"Fetching data for {district.get('name')} - {api_url}")  
+                    
+                    weather_response = requests.get(api_url)
+                    weather_response.raise_for_status()  
+
+                    weather_data = weather_response.json()
+
+                    # print("data", weather_data) 
+
+                    hourly_data = weather_data.get('hourly', {})
+                    temperature_2m_values = hourly_data.get('temperature_2m', [])
+
+                    # print(f"Temperature 2m values: {temperature_2m_values}")  
+
+                    
+                    avg_temp_2pm = sum(temperature_2m_values) / len(temperature_2m_values) if temperature_2m_values else None
+
+                    if avg_temp_2pm is not None:
+                        coolest_districts.append({
+                            'district_name': district.get('name'), 
+                            'average_temperature_2pm': avg_temp_2pm
+                        })
+
+            
+            coolest_districts.sort(key=lambda x: x['average_temperature_2pm'])
+            top_10_coolest_districts = coolest_districts[:10]
+
+            return Response({'coolest_districts': top_10_coolest_districts}, status=status.HTTP_200_OK)
+
+        except requests.exceptions.HTTPError as err:
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
