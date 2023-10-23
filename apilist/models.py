bd77da111d5dcb1f07c0615a06876cbe2af40f95
Afroza Nowshin Senjuti <afroza.nowshin@adndiginet.com>
2023-10-23 14:45:34 +0600

    user model added

--- apilist/models.py
+++ apilist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.hashers import make_password
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=20, default='meow')
+
+    def save(self, *args, **kwargs):
+        # Encrypt the password before saving to the database
+        self.password = make_password(self.password)
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
