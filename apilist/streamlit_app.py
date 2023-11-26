d86d64af7c3b4ffb149f129099aa84df6b6ec6c1
Afroza Nowshin <afroza.nowshin@gmail.com>
2023-11-26 22:08:57 +0600

    streamlit logic changed

--- apilist/streamlit_app.py
+++ apilist/streamlit_app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 import requests
+from django.core.cache import cache
+from datetime import datetime
 
 st.title("Predicting Temperature of Dhaka")
 
 # User input (date)
-date_input = st.date_input('Select a date')
+date_input_str = st.date_input('Select a date')
 
 # Submit button
 if st.button('Submit'):
-    # Make API call to Django API
-    api_url = "http://127.0.0.1:8000/api/predict-weather/"
-    params = {'date': str(date_input)}
-    response = requests.post(api_url, data=params)
+    # Convert the input date string to numerical features
+    date_input = datetime.strptime(str(date_input_str), "%Y-%m-%d")
+    input_features = [date_input.year, date_input.month, date_input.day]
 
-    # Display API response
-    if response.status_code == 200:
-        result = response.json()
-        predicted_temperature = result["predicted_temperature"]
-        # Format the temperature to two decimal places
-        formatted_temperature = "{:.2f}°C".format(predicted_temperature)
-        st.write(f'Predicted Temperature: {formatted_temperature}')
+    # Retrieve the model from the cache
+    cached_model = cache.get('serialized_model')
+
+    if cached_model:
+        try:
+            # Make predictions using the cached model
+            predictions = cached_model.predict([input_features])
+            # Extract the predicted temperature
+            predicted_temperature = predictions[0]  # Assuming the prediction is a single value
+
+            # Format the temperature to two decimal places
+            formatted_temperature = "{:.2f}°C".format(predicted_temperature)
+
+            # Display the result
+            st.write(f'Predicted Temperature: {formatted_temperature}')
+        except Exception as e:
+            st.error(f'Error making predictions: {e}')
     else:
-        st.error('Error fetching data from the API')
+        st.error('Model not found in the cache. Please make sure the model is loaded.')
+
+# Note: Ensure you have 'cache' properly configured in your Streamlit app (e.g., using st.cache decorator)
